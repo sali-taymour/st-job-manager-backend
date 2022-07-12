69ac149f1b7323a9c950f0d15d5b81a94f3c119b
sali <sali.taymor@gmail.com>
2022-07-12 10:50:07 +0200

    added env and connected with Mongo Atlas

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { JobSource } from "./models/JobSource.js";
+
+dotenv.config();
+
+const MONGODB_URI =
+    process.env.MONGODB_URI || "mongodb://localhost/ejt-job-manager";
+
+mongoose.connect(MONGODB_URI, (err) => {
+    if (err) {
+        console.log({
+            error: "Cannot connect to MongoDB database.",
+            err: `"${err}"`,
+        });
+    }
+});
 
 const app = express();
 const port = process.env.PORT || 3044;
